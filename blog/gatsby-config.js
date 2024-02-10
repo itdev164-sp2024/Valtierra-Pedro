c2a77da465099755e5a1e697b9e312638e96a7d0
valtiep1 <valtiep1@gmatc.matc.edu>
2024-02-10 08:51:46 -0600

    Completed Activity 1

---
+++ blog/gatsby-config.js
@@ -0,0 +1,57 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Blog`,
+    description: ``,
+    author: `Pedro Ivan Valtierra`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: `Pedro Valtierra`,
+      company: `Blogs.com`,
+      address: `PO BOX 2529`
+    }
+  },
+  plugins: [
+
+    {
+resolve: `gatsby-source-contentful`,
+options: {
+spaceId:`${process.env.SPACE_ID}`,
+accessToken:`${process.env.ACCESS_TOKEN}`
+    }
+  },
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
